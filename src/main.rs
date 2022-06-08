b5595af95bfd33f18f2ffbed35b02c798440a5b6
Staninna <26458805+Staninna@users.noreply.github.com>
2022-06-08 10:53:41 +0200

    ✨ Now bootable with qemu and will rick-roll you great sucess

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,22 @@
 // Imports
 use core::panic::PanicInfo;
 
-// Entry point
+// Put I love mom in the memory at 0xb8000
+static MOM: &[u8] = b"Never gonna give you up                                                         Never gonna let you down                                                        Never gonna run around and desert you                                           Never gonna make you cry                                                        Never gonna say goodbye                                                         Never gonna tell a lie and hurt you";
+
+// Entry point, since the linker looks for a function
+// named `_start` by default
 #[no_mangle] // Make function name not scrambled at compile time
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in MOM.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
